ab0a4b4e741bc18f71fa24b2d5b0ff393c37f37d
Zafar Kudratov <zafarkudratov@gmail.com>
2019-10-07 23:55:01 +0300

    refactoring

--- contact-list/src/components/ContactList/ContactList.jsx
+++ contact-list/src/components/ContactList/ContactList.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import ContactListItem from './ContactListItem'
+import ContactListItem from '../ContactListenItem/ContactListItem'
+import './ContactList.css'
 
 class ContactList extends Component {
     render() {
-        const { contacts, name, surname, phoneNumber } = this.props
+        const { contacts, name, surname, phoneNumber, age } = this.props
         return (
             <React.Fragment>
                 <h4>ContactList: {this.props.contacts.length}</h4>
                 { this.props.contacts.map(contact => (
-                            <ContactListItem  key={contact.id} contact={contact}/>
+                            <ContactListItem  
+                                key={contact.id} 
+                                contact={contact}
+                            />
                          )
                     )
                 }                
